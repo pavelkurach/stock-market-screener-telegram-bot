2f831b3641d8e2a216e35af36efe0be4e0849d19
pavelkurach <pavel.kurach1@gmail.com>
2022-12-24 20:54:11 +0100

    added support of different indices and more interactivity

--- stock_market_database.py
+++ stock_market_database.py
@@ -3,48 +3,70 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import MetaData
 import yfinance as yf
 
 
 class StockPriceHistoryDatabase:
     WIKI = 'https://en.wikipedia.org/wiki/'
 
-    def __init__(self, indices=['CAC40']):
+    def __init__(self, indices=['CAC40', 'S&P500', 'FTSE100', 'DAX']):
 
         self.indices = indices
         self.tickers = []
 
         # Get the list of companies in the indices
         if 'CAC40' in self.indices:
-            self.tickers.extend(
-                pd.read_html(self.WIKI + 'CAC_40', flavor='html5lib')[4]['Ticker'].to_list())
+            self.tickers.extend(pd.read_html(self.WIKI + 'CAC_40',
+                                             flavor='html5lib')[4]['Ticker'].to_list())
 
-        print(self.tickers)
+        if 'S&P500' in self.indices:
+            self.tickers.extend(pd.read_html(self.WIKI + 'List_of_S%26P_500_companies',
+                                             flavor='html5lib')[0]['Symbol'].to_list())
+
+        if 'FTSE100' in self.indices:
+            self.tickers.extend(pd.read_html(self.WIKI + 'FTSE_100_Index',
+                                             flavor='html5lib')[4]['EPIC'].to_list())
+
+        if 'DAX' in self.indices:
+            self.tickers.extend(pd.read_html(self.WIKI + 'DAX',
+                                             flavor='html5lib')[4]['Ticker'].to_list())
 
         # Create database engine
-        self.engine = create_engine(f'postgresql://pavelkurach@localhost:5432/stock_price_history')
+        self.engine = create_engine(
+            f'postgresql://pavelkurach@localhost:5432/stock_price_history')
         if not database_exists(self.engine.url):
             create_database(self.engine.url)
+        else:
+            # Clear database
+            metadata = MetaData(bind=self.engine)
+            metadata.reflect()
+            metadata.drop_all()
+            self.engine.dispose()
+
         print("Successfully created a database")
 
         # Get stock market data
-        stock_market_data = []
         for ticker in self.tickers:
-            stock_market_data.append(yf.download(tickers=ticker,
-                                                 period="5d",
-                                                 interval="5m").reset_index().iloc[:-20, :])
+            frame = yf.download(tickers=ticker,
+                                period="5d",
+                                interval="5m").reset_index().iloc[:-20, :]
 
-        # Put stock market data to the database and add a primary key
-        # Primary key is needed for sqlalchemy automap
-        for frame, ticker in zip(stock_market_data, self.tickers):
-            frame.to_sql(ticker, self.engine, if_exists='replace', index=False)
-            with self.engine.connect() as con:
-                con.execute(f'ALTER TABLE "{ticker}" ADD PRIMARY KEY ("{frame.columns[0]}");')
+            if (not frame.empty) and 'Datetime' in frame.columns:
+                frame.to_sql(ticker, self.engine, if_exists='replace', index=False)
+                with self.engine.connect() as con:
+                    con.execute(
+                        f'ALTER TABLE "{ticker}" ADD PRIMARY KEY ("{frame.columns[0]}");')
+            else:
+                self.tickers.remove(ticker)
 
         # Create an object-relation mapping with sqlalchemy automap
         self.base = automap_base()
         self.base.prepare(autoload_with=self.engine)
-        self.data_classes = {ticker: cls for ticker, cls in zip(self.tickers, self.base.classes)}
+        self.data_classes = {}
+        for cls in self.base.classes:
+            self.data_classes[cls.__name__] = cls
+        self.tickers = list(self.data_classes.keys())
 
         # Start a session
         self.session = Session(self.engine)
@@ -53,7 +75,7 @@ class StockPriceHistoryDatabase:
         self.sigma = {}
         for ticker in self.tickers:
             query = self.session.query(self.data_classes[ticker])
-            sigma_df = pd.read_sql(query.statement, query.session.bind, index_col='Datetime')
+            sigma_df = pd.read_sql(query.statement, query.session.bind)
             self.sigma[ticker] = sigma_df.Close.pct_change().std()
 
         # Get datetime of the latest ticks
@@ -75,71 +97,51 @@ class StockPriceHistoryDatabase:
                 self.session.commit()
                 self.updates[ticker] = tick['Datetime']
 
-    def fetch_shock_history(self, user_updates: dict, user_indices: list = ['CAC40']) -> list:
+    def fetch_shock_history(self, user_tickers: list, user_updates: dict) -> list:
         """
         Retrieving stock market shock updates
-        :param user_indices: list of indices that contain companies user want to be notified about
+        :param user_tickers: list of companies user want to be notified about
         :param user_updates: contains datetime for each ticker, the function notifies about
         shocks happening after this datetime
         :return: list of string (each string contains a message about a recent market shock)
         """
-        user_tickers = []
         result = []
 
-        if 'CAC40' in user_indices:
-            user_tickers.extend(
-                pd.read_html(self.WIKI + 'CAC_40', flavor='html5lib')[4]['Ticker'].to_list())
-
         for ticker in user_tickers:
-            query = self.session.query(self.data_classes[ticker]).filter(
-                self.data_classes[ticker].Datetime >= user_updates[ticker])
-            ticker_data = pd.read_sql(query.statement, query.session.bind)
+            if ticker in self.tickers:
+                query = self.session.query(self.data_classes[ticker]).filter(
+                    self.data_classes[ticker].Datetime >= user_updates[ticker])
+                ticker_data = pd.read_sql(query.statement, query.session.bind)
 
-            last_close = None
+                last_close = None
 
-            for _, tick in ticker_data.iterrows():
-                if _ == 0:
+                for _, tick in ticker_data.iterrows():
+                    if _ == 0:
+                        last_close = tick["Close"]
+                        continue
+
+                    change_ratio = (tick["Close"] / last_close - 1) / self.sigma[ticker]
+                    if change_ratio > 3:
+                        result.append(
+                            f'{ticker} stock price moved by '
+                            f'{(tick["Close"] / last_close - 1) * 100:.2f}% '
+                            f'({change_ratio:.2f}x volatility) '
+                            f'from {user_updates[ticker].strftime("%H:%M")} '
+                            f'to {tick["Datetime"].strftime("%H:%M")}.')
                     last_close = tick["Close"]
-                    continue
-
-                change_ratio = (tick["Close"] / last_close - 1) / self.sigma[ticker]
-                if change_ratio > 3:
-                    result.append(
-                        f'{ticker} stock price moved by '
-                        f'{(tick["Close"] / last_close - 1) * 100:.2f}% '
-                        f'({change_ratio:.2f}x volatility) '
-                        f'from {user_updates[ticker].strftime("%H:%M")} '
-                        f'to {tick["Datetime"].strftime("%H:%M")}.')
-                last_close = tick["Close"]
-                user_updates[ticker] = tick['Datetime']
+                    user_updates[ticker] = tick['Datetime']
 
         return result
 
-    def print_first_table(self):
-        query = self.session.query(self.data_classes['AIR.PA'])
-        print(pd.read_sql(query.statement, query.session.bind))
+    def print_tables(self):
+        for ticker in self.tickers:
+            query = self.session.query(self.data_classes[ticker])
+            print(ticker)
+            print(pd.read_sql(query.statement, query.session.bind))
 
 
 def main():
-    db = StockPriceHistoryDatabase()
-
-    user_tickers = pd.read_html(
-        'https://en.wikipedia.org/wiki/CAC_40', flavor='html5lib')[4]['Ticker'].to_list()
-
-    user_updates = {ticker: '2022-12-23 10:00:00'
-                    for ticker in user_tickers}
-    messages = db.fetch_shock_history(user_updates=user_updates)
-    for msg in messages:
-        print(msg)
-
-    print("before update")
-    db.print_first_table()
-    db.update_database()
-    print("after update")
-    db.print_first_table()
-    print("sigmas")
-    print(db.sigma)
-
+    pass
 
 
 if __name__ == '__main__':
--- telegram_bot.py
+++ telegram_bot.py
@@ -7,6 +7,8 @@ from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from tokens import BOT_TOKEN
 
+WIKI = 'https://en.wikipedia.org/wiki/'
+
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -15,33 +17,103 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def monitor(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
+    if not ('indices' in context.user_data.keys()):
+        context.user_data['indices'] = []
+        context.user_data['tickers'] = []
+        context.user_data['updates'] = {}
 
-    context.user_data['indices'] = ["CAC40"]
-    context.user_data['tickers'] = pd.read_html(
-        'https://en.wikipedia.org/wiki/CAC_40', flavor='html5lib')[4]['Ticker'].to_list()
-    context.user_data['updates'] = {ticker: '2022-12-23 10:00:00'
-                                    for ticker in context.user_data['tickers']}
+    args = context.args
+    args = [arg.upper() for arg in args]
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Monitoring CAC40 companies.')
+    msg = []
+
+    for index in args:
+        if index in TICKERS_DICT.keys():
+            if index in context.user_data['indices']:
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text=f'Already monitoring {index} companies.')
+            else:
+                context.user_data['indices'].append(index)
+                context.user_data['tickers'].extend(TICKERS_DICT[index])
+                msg.append(index)
+                for ticker in TICKERS_DICT[index]:
+                    # It should be "datetime.now", I put another datetime for test only
+                    context.user_data['updates'][ticker] = '2022-12-23 10:00:00'
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=f'{index} is not a supported index.')
+
+    if msg:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Start monitoring companies from the following indices: ' +
+                                      ', '.join(msg) + '.')
 
     context.job_queue.run_repeating(notify_about_latest_shocks, interval=30, first=5,
                                     context={'updates': context.user_data['updates'],
+                                             'tickers': context.user_data['tickers'],
                                              'chat_id': update.message.chat_id})
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
+def stop(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+
+    if not ('indices' in context.user_data.keys()) or context.user_data['indices'] == []:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='You have not started monitoring yet.')
+        return
+
+    args = context.args
+    if not args:
+        context.user_data['indices'] = []
+        context.user_data['tickers'] = []
+        context.user_data['updates'] = {}
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Stopped monitoring any company.')
+
+    args = [arg.upper() for arg in args]
+
+    msg = []
+
+    for index in args:
+        if index in TICKERS_DICT.keys():
+            if index in context.user_data['indices']:
+                context.user_data['indices'].remove(index)
+                msg.append(index)
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text=f'You are not monitoring {index} companies.')
+
+    context.user_data['tickers'] = []
+    for index in context.user_data['indices']:
+        context.user_data['tickers'].extend(TICKERS_DICT[index])
+
+    if msg:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Stopped monitoring companies from the following indices: ' +
+                                      ', '.join(msg) + '.')
+
+
+def help(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text(
+        'To start monitoring, use the following command:'
+        '\r\n/monitor index1 index2 …\r\n\r\n'
+        'Example: \r\n/monitor S&P500\r\n\r\n'
+        'To stop monitoring, use the following commands:\r\n'
+        '/stop index1 index2 …\r\n/stop \r\n\r\nExample: \r\n'
+        '/stop S&P500\r\n')
 
 
+# noinspection PyUnresolvedReferences
 def notify_about_latest_shocks(context: CallbackContext) -> None:
     """
     Notify the user about the latest stock market shocks
     """
     print(context.job.context['updates'])
-    messages = db.fetch_shock_history(user_updates=context.job.context['updates'])
+    messages = db.fetch_shock_history(user_tickers=context.job.context['tickers'],
+                                      user_updates=context.job.context['updates'])
     if messages:
         for msg in messages:
             context.bot.send_message(chat_id=context.job.context['chat_id'],
@@ -62,6 +134,19 @@ def main() -> None:
     global db
     db = stock_market_database.StockPriceHistoryDatabase()
 
+    # Get tickers lists
+    global TICKERS_DICT
+    cac40 = pd.read_html(WIKI + 'CAC_40', flavor='html5lib')[4]['Ticker'].to_list()
+    sp500 = pd.read_html(WIKI + 'List_of_S%26P_500_companies',
+                         flavor='html5lib')[0]['Symbol'].to_list()
+    ftse100 = pd.read_html(WIKI + 'FTSE_100_Index', flavor='html5lib')[4]['EPIC'].to_list()
+    dax = pd.read_html(WIKI + 'DAX', flavor='html5lib')[4]['Ticker'].to_list()
+    TICKERS_DICT = {
+        'CAC40': cac40,
+        'S&P500': sp500,
+        'FTSE100': ftse100,
+        'DAX': dax
+    }
     # Create the Updater and pass it your bot's token.
     updater = Updater(BOT_TOKEN)
 
@@ -75,8 +160,9 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("monitor", monitor))
+    dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("stop", stop))
 
     # Start the Bot
     updater.start_polling()
